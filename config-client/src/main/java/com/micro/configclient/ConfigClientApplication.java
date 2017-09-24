0ca22a212b894ab1f352ddda6cf5e4eaf7d630af
fqc <337940626@qq.com>
2017-09-24 21:20:39 +0800

    config-service as config-client

--- config-client/src/main/java/com/micro/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/micro/configclient/ConfigClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 }
