d7b9c4bb9f361687886d4441c82a7683f8ef4a26
fqc <337940626@qq.com>
2017-10-15 15:52:12 +0800

    fix last commit

--- eureka-server/src/main/java/com/micro/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/micro/eurekaserver/EurekaServerApplication.java
@@ -2,11 +2,10 @@ package com.micro.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //spring cloud版本升级后 内部已经包含了@EnableDiscoveryClient
 //@EnableDiscoveryClient
 public class EurekaServerApplication {
 
